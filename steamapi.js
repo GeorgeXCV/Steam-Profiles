00d4e30509fb606ff565d367c04d215b1dbeba62
GeorgeXCV <georgeashton95@gmail.com>
2020-10-17 12:58:58 +0100

    Compression routes

--- steamapi.js
+++ steamapi.js
@@ -5,9 +5,11 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
 const database = require('./database');
+const compression = require('compression');
 
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(express.static(__dirname));
+app.use(compression()); //Compress all routes
 
 app.locals.moment = require('moment');
 
